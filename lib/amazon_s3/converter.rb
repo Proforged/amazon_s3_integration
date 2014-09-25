6828a2029f9b8a77182bc8982f873b977749e3f4
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-09-25 18:11:05 -0300

    Adds json_path, generate_csv

--- lib/amazon_s3/converter.rb
+++ lib/amazon_s3/converter.rb
@@ -1,18 +1,49 @@
 class Converter
   class << self
-    def header(target, prefix = nil)
+    def csv_header(target, prefix = nil)
       case target
       when Hash
         target.map do |key, value|
-          header(value, prepare_prefix(prefix, key))
+          csv_header(value, prepare_prefix(prefix, key))
         end.flatten
       when Array
         target.each_with_index.map do |object, i|
-          header(object, prepare_prefix(prefix, i))
+          csv_header(object, prepare_prefix(prefix, i))
         end.flatten
       else
         prefix.to_s
       end
+    end
+
+    def json_path(json, path)
+      case json
+      when Hash
+        json = json.with_indifferent_access
+        json_path(json[leftmost_path(path)], rest_of_path(path))
+      when Array
+        json_path(json[leftmost_path(path).to_i], rest_of_path(path))
+      else
+        path == "" ? json.to_s : "" # trying to traverse even more?
+      end
+    end
+
+    def generate_csv(worst_case_json)
+      header = csv_header(worst_case_json)
+
+      output = header.inject([]) do |buff, column|
+        buff.push json_path(worst_case_json, column)
+      end
+
+      [header.join(","), output.join(",")].join("\n")
+    end
+
+    private
+    def rest_of_path(path)
+      path.split(".")[1..-1].join(".")
+    end
+
+    def leftmost_path(path)
+      path.split(".")[0]
     end
 
     def prepare_prefix(a , b)
--- spec/amazon_s3/converter_spec.rb
+++ spec/amazon_s3/converter_spec.rb
@@ -3,39 +3,97 @@ require 'spec_helper'
 describe Converter do
   subject { described_class }
 
-  describe '.json_to_csv' do
-    let(:json) do
-      {
-        id: "R154085346541340",
-        string: "USD",
-        hash_nested: {
-          adjustment: 20,
-          tax: 10,
-          shipping: {
-            airplane: 5,
-            land: {
-              north: 1,
-              south: 2
-            }
+  let(:worst_case_json) do
+    {
+      id: "R154085346541340",
+      string: "USD",
+      hash_nested: {
+        adjustment: 20,
+        tax: 10,
+        shipping: {
+          airplane: 5,
+          land: {
+            north: 1,
+            south: 2
           }
-        },
-        array_of_hashes: [{
-          id: 1,
-          status: "shipped"
-        }, {
-          id: 2,
-          status: "ready"
-        }],
-        array: ["red", "green", "refactor"],
-        array_with_hash: [10, { a: 1, b: 2 }],
-        array_with_nested_array: ["yes", ["no", "nope"], [[["maybe"]]]]
-      }
-    end
+        }
+      },
+      array_of_hashes: [{
+        id: 1,
+        status: "shipped"
+      }, {
+        id: 2,
+        status: "ready"
+      }],
+      array: ["red", "green", "refactor"],
+      array_with_hash: [10, { a: 1, b: 2 }],
+      array_with_nested_array: ["yes", ["no", "nope"], [[["maybe"]]]]
+    }
+  end
 
-    it 'works' do
-      expect(subject.header(json)).to eq([
+  describe '.csv_header' do
+    it 'generates a header by flattening the json' do
+      expect(subject.csv_header(worst_case_json)).to eq([
         "id", "string", "hash_nested.adjustment", "hash_nested.tax", "hash_nested.shipping.airplane", "hash_nested.shipping.land.north", "hash_nested.shipping.land.south", "array_of_hashes.0.id", "array_of_hashes.0.status", "array_of_hashes.1.id", "array_of_hashes.1.status", "array.0", "array.1", "array.2", "array_with_hash.0", "array_with_hash.1.a", "array_with_hash.1.b", "array_with_nested_array.0", "array_with_nested_array.1.0", "array_with_nested_array.1.1", "array_with_nested_array.2.0.0.0"
       ])
+    end
+  end
+
+  describe '.json_path' do
+    tests = {
+      "id"                              => "R154085346541340",
+      "string"                          => "USD",
+      "hash_nested.adjustment"          => "20",
+      "hash_nested.tax"                 => "10",
+      "hash_nested.shipping.airplane"   => "5",
+      "hash_nested.shipping.land.north" => "1",
+      "hash_nested.shipping.land.south" => "2",
+      "array_of_hashes.0.id"            => "1",
+      "array_of_hashes.0.status"        => "shipped",
+      "array_of_hashes.1.id"            => "2",
+      "array_of_hashes.1.status"        => "ready",
+      "array.0"                         => "red",
+      "array.1"                         => "green",
+      "array.2"                         => "refactor",
+      "array_with_hash.0"               => "10",
+      "array_with_hash.1.a"             => "1",
+      "array_with_hash.1.b"             => "2",
+      "array_with_nested_array.0"       => "yes",
+      "array_with_nested_array.1.0"     => "no",
+      "array_with_nested_array.1.1"     => "nope",
+      "array_with_nested_array.2.0.0.0" => "maybe"
+    }
+
+    tests.each do |path, value|
+      it "navigates through the path #{path}" do
+        expect(subject.json_path(worst_case_json, path)).to eq value
+      end
+    end
+
+    context 'value not found for path' do
+      tests = [
+        "nothing",
+        "nothing.really.nada",
+        "hash_nested.shipping.0.north",
+        "array_of_hashes.0.id.0.1",
+        "0.0.0.0"
+      ]
+
+      tests.each do |path|
+        it "returns \"\" for #{path}" do
+          expect(subject.json_path(worst_case_json, path)).to eq ""
+        end
+      end
+    end
+
+    describe '.generate_csv' do
+      let(:csv_fixture) do
+        "id,string,hash_nested.adjustment,hash_nested.tax,hash_nested.shipping.airplane,hash_nested.shipping.land.north,hash_nested.shipping.land.south,array_of_hashes.0.id,array_of_hashes.0.status,array_of_hashes.1.id,array_of_hashes.1.status,array.0,array.1,array.2,array_with_hash.0,array_with_hash.1.a,array_with_hash.1.b,array_with_nested_array.0,array_with_nested_array.1.0,array_with_nested_array.1.1,array_with_nested_array.2.0.0.0\nR154085346541340,USD,20,10,5,1,2,1,shipped,2,ready,red,green,refactor,10,1,2,yes,no,nope,maybe"
+      end
+
+      it 'transforms json into a flat csv' do
+        expect(subject.generate_csv(worst_case_json)).to eq csv_fixture
+      end
     end
   end
 end
