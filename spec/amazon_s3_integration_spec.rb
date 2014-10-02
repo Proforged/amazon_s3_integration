50f24cd36b057db6f350c441f07bf8364bd7d14a
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-10-02 18:43:27 -0300

    DRY up requests in integration specs

--- spec/amazon_s3_integration_spec.rb
+++ spec/amazon_s3_integration_spec.rb
@@ -6,23 +6,26 @@ describe AmazonS3Integration do
     JSON.parse(last_response.body)
   end
 
-  describe 'POST /export_file' do
-    let(:request) do
-      {
-        request_id: '1234567',
-        parameters: {
-          access_key_id: aws_testing[:access_key_id],
-          secret_access_key: aws_testing[:secret_access_key],
-          region: 'us-east-1',
-          bucket_name: aws_testing[:bucket_name],
-          file_name: 'files/shipment.csv'
-        },
-        shipment: sample_shipment("R9")
+  let(:common_request) do
+    {
+      request_id: '1234567',
+      parameters: {
+        access_key_id: aws_testing[:access_key_id],
+        secret_access_key: aws_testing[:secret_access_key],
+        region: 'us-east-1',
+        bucket_name: aws_testing[:bucket_name],
       }
-    end
+    }
+  end
 
+  describe 'POST /export_file' do
     it 'saves to S3 and returns a summary (200)', :vcr do
-      post '/export_file', request.to_json, {}
+      post '/export_file', common_request.deep_merge({
+        parameters: {
+          file_name: 'files/shipment.csv',
+          shipment: sample_shipment("R9")
+        }
+      }).to_json, {}
 
       expect(json_response["summary"]).to eq "File files/shipment.csv was saved to S3"
       expect(last_response.status).to eq 200
@@ -30,7 +33,7 @@ describe AmazonS3Integration do
 
     context 'bucket does not exist' do
       it 'returns a message (500)', :vcr do
-        post '/export_file', request.deep_merge(parameters: { bucket_name: 'not-to-be-found' }).to_json, {}
+        post '/export_file', common_request.deep_merge(parameters: { bucket_name: 'not-to-be-found' }).to_json, {}
 
         expect(json_response["summary"]).to eq "Bucket 'not-to-be-found' was not found."
         expect(last_response.status).to eq 500
@@ -40,16 +43,11 @@ describe AmazonS3Integration do
     context 'when batch request' do
       let(:request) do
         {
-          request_id: '1234567',
           parameters: {
-            access_key_id: aws_testing[:access_key_id],
-            secret_access_key: aws_testing[:secret_access_key],
-            region: 'us-east-1',
-            bucket_name: aws_testing[:bucket_name],
             file_name: 'files/shipment_batch.csv'
           },
           shipments: [sample_shipment("R9"), sample_shipment("R1")]
-        }
+        }.deep_merge(common_request)
       end
 
       it 'saves to S3 and returns a summary (200)', :vcr do
@@ -64,16 +62,11 @@ describe AmazonS3Integration do
   describe 'POST /import_file' do
     let(:request) do
       {
-        request_id: '1234567',
         parameters: {
-          access_key_id: aws_testing[:access_key_id],
-          secret_access_key: aws_testing[:secret_access_key],
-          region: 'us-east-1',
-          bucket_name: 'bruno-s3-testing',
           file_name: 'files/shipment_batch.csv',
           object_type: 'shipment'
         }
-      }
+      }.deep_merge(common_request)
     end
 
     it 'reads from S3 and returns object and summary (200)', :vcr do
